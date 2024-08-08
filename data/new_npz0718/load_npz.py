a0ef6de3befadd5f6615aa13d472319de16bf812
kathy5311 <kathy531@hanyang.ac.kr>
2024-08-08 16:22:24 +0900

    NewCommit: open featurize npz file

---
+++ data/new_npz0718/load_npz.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+f='./src/new_npz0718/test_subset_rev0718_2.npz'
+
+data=np.load(f, allow_pickle=True)
+
+#print(list(data.keys()))
+
+#print(len(data['tags']))
+#print(data['features'])
+
+for i in list(data['features'][0].keys()):
+    print(i)
+    print(data['features'][-1][i])
+    print(data['features'][-1][i].shape)
+    print()
